80d8e960ecacdc81f88cab45ad7067d4bc6b7404
gosselvadim-spec <gosselvadim@gmail.com>
2026-09-19 15:12:59 +1000

    измен текст

--- MYGAME (весь код).py
+++ MYGAME (весь код).py
@@ -6,7 +6,7 @@ pygame.init()
 SCREEN_WIDTH = 1500
 SCREEN_HEIGHT = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Menu")
+pygame.display.set_caption("Menushka")
 
 # ИНИЦИАЛИЗАЦИЯ ЗВУКА
 pygame.mixer.init()
